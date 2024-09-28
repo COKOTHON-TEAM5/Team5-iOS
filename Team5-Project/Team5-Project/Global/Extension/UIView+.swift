03497eedc01593aa0d1a088eddbb588e78277f25
kwonjeong <kwonjeong9470@gmail.com>
2024-09-29 01:05:09 +0900

    #6 add: UIView 익스텐션 추가

--- Team5-Project/Team5-Project/Global/Extension/UIView+.swift
+++ Team5-Project/Team5-Project/Global/Extension/UIView+.swift
@@ -12,4 +12,17 @@ extension UIView {
     func addSubviews(_ views: UIView...) {
         views.forEach { self.addSubview($0) }
     }
+    
+    func makeCornerRound (radius : CGFloat) {
+        layer.cornerRadius = radius
+        layer.masksToBounds = true
+    }
+    
+    func makeShadow (radius : CGFloat, offset : CGSize, opacity : Float){
+        layer.shadowColor = UIColor.darkGray.cgColor
+        layer.shadowOffset = offset
+        layer.shadowRadius = radius
+        layer.shadowOpacity = opacity
+        layer.masksToBounds = false
+    }
 }
